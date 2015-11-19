162f895ffcf227ede399ac31993cdaf5d1a2e35b
Nick De Veaux <deveaux@recombine.com>
2015-11-18 20:58:55 -0500

    worker connects to dynamoDB

--- worker_reader.py
+++ worker_reader.py
@@ -11,3 +11,37 @@ for message in worker_queue.receive_messages(MessageAttributeNames=['Url']):
     if message.message_attributes is not None:
         url = message.message_attributes.get('Url').get('StringValue')
         print 'Worker Message received: {0}: {1}'.format(message.body, url)
+
+
+# Get the service resource.
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.Table('Worker_Output')
+
+print(table.creation_date_time)
+
+table.put_item(
+   Item={
+        'username': 'janedoe',
+        'first_name': 'Jane',
+        'last_name': 'Doe',
+        'age': 25,
+        'url': 'http://facebook.com',
+        'account_type': 'standard_user',
+    }
+)
+
+response = table.get_item(
+    Key={
+    	'url': 'http://facebook.com'
+    }
+)
+item = response['Item']
+print(item)
+
+"""table.delete_item(
+    Key={
+    	'url': 'http://facebook.com'
+    }
+)"""
+
