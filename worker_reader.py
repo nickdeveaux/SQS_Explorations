c9eacffa58ec6b733bd575310cb4e6b81af20c5c
Nick De Veaux <deveaux@recombine.com>
2015-11-18 21:01:11 -0500

    writes output to db in a separate function

--- worker_reader.py
+++ worker_reader.py
@@ -1,47 +1,45 @@
 import boto3
 
-# Get the service resource
-sqs = boto3.resource('sqs')
+def write_to_db(url):
+    # Get the service resource.
+    dynamodb = boto3.resource('dynamodb')
+    
+    table = dynamodb.Table('Worker_Output')
+    
+    table.put_item(
+       Item={
+            'username': 'janedoe',
+            'first_name': 'Jane',
+            'last_name': 'Doe',
+            'age': 25,
+            'url': url,
+            'account_type': 'standard_user',
+        }
+    )
+    
+    response = table.get_item(
+        Key={
+            'url': url
+        }
+    )
+    item = response['Item']
+    print(item)
 
-# Get the queue
-worker_queue = sqs.get_queue_by_name(QueueName='worker_input')
+def main():
+    
+    # Get the service resource
+    sqs = boto3.resource('sqs')
+    
+    # Get the queue
+    worker_queue = sqs.get_queue_by_name(QueueName='worker_input')
+    
+    for message in worker_queue.receive_messages(MessageAttributeNames=['Url']):
+        # TODO replace this with worker code
+        if message.message_attributes is not None:
+            url = message.message_attributes.get('Url').get('StringValue')
+            print 'Worker Message received: {0}: {1}'.format(message.body, url)
+            write_to_db(url)
 
-for message in worker_queue.receive_messages(MessageAttributeNames=['Url']):
-    # TODO replace printing with worker code
-    if message.message_attributes is not None:
-        url = message.message_attributes.get('Url').get('StringValue')
-        print 'Worker Message received: {0}: {1}'.format(message.body, url)
-
-
-# Get the service resource.
-dynamodb = boto3.resource('dynamodb')
-
-table = dynamodb.Table('Worker_Output')
-
-print(table.creation_date_time)
-
-table.put_item(
-   Item={
-        'username': 'janedoe',
-        'first_name': 'Jane',
-        'last_name': 'Doe',
-        'age': 25,
-        'url': 'http://facebook.com',
-        'account_type': 'standard_user',
-    }
-)
-
-response = table.get_item(
-    Key={
-    	'url': 'http://facebook.com'
-    }
-)
-item = response['Item']
-print(item)
-
-"""table.delete_item(
-    Key={
-    	'url': 'http://facebook.com'
-    }
-)"""
+if __name__ == '__main__':
+    main()
 
